31bf478cde605da40966e8d89de0f7db920c68fe
jaejunha <dreamline91@naver.com>
2018-07-18 11:52:10 +0900

    ifconfig -> iwconfig

--- Client/client.py
+++ Client/client.py
@@ -1,14 +1,18 @@
 import os 
-import re
-p = re.compile('.*[^:]Link')
 
 def getInterface():
 	list_interface = []
-	list_result = os.popen('ifconfig').read().split('\n')
+	print ''
+	print 'Checking Interfaces:'
+	print '========================================================='
+	list_result = os.popen('iwconfig').read().split('\n')
 	for str_result in list_result:
-		m = p.match(str_result)
-		if m:
-			list_interface.append(m.group().split(' ')[0])
+		interface = str_result.split(' ')[0]
+		if str_result.find('no wireless') < 0 and interface:
+			print str_result	
+			list_interface.append(interface)
+	print '========================================================='
+	print ''
 	return list_interface
 
 def searchESSID(list_interface):
@@ -16,18 +20,19 @@ def searchESSID(list_interface):
 	for str_interface in list_interface:
 		try:
 			list_result = os.popen('iwlist ' + str_interface + ' scanning').read()
-			if list_result.find('doesn\'t support scanning') == -1:
-				list_result = list_result.split('\n')
-				for str_result in list_result:
-					if str_result.find('ESSID:') > 0:
-						list_essid.append(str_result.split('ESSID:')[1][1:-1])
+			list_result = list_result.split('\n')
+			for str_result in list_result:
+				if str_result.find('ESSID:') > 0:
+					list_essid.append(str_result.split('ESSID:')[1][1:-1])
 		except Exception:
 			pass
 	return list_essid
 
 def printESSID(list_essid):
+	print 'Conntectable AP List:'
+	print '========================================================='
         for essid in list_essid:
                 print essid
-
+	print '========================================================='
 list_essid = searchESSID(getInterface())
 printESSID(list_essid)
