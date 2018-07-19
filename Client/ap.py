fd821c7af685d78d7aeecfda9c6e1e8b35052c6c
jaejunha <dreamline91@naver.com>
2018-07-19 14:40:07 +0900

    change ap

--- Client/ap.py
+++ Client/ap.py
@@ -1,38 +1,54 @@
-import os 
+import os
+
+def giveAlert(str_error):
+	list_command = {'arg_s': 'Help > python client.py scan <interface name>',
+        		'status': 'Help > Check your interface status',
+			'arg_c': 'Help > python client.py change <interface name> <AP id>',
+			'input': 'Help > Check your input',
+        		}
+        print list_command[str_error]
 
 def getInterface():
-	list_interface = []
-	print ''
-	print 'Checking Interfaces:'
-	print '========================================================='
-	list_result = os.popen('iwconfig').read().split('\n')
+	file_in, file_out, file_error = os.popen3('iwconfig')
+	list_result = file_out.read().split('\n')
 	for str_result in list_result:
-		interface = str_result.split(' ')[0]
-		if str_result.find('no wireless') < 0 and interface:
-			print str_result	
-			list_interface.append(interface)
-	print '========================================================='
-	print ''
-	return list_interface
+		if 'ESSID' in str_result:
+			str_interface = str_result.split(' ')[0]
+			str_essid = str_result.split('ESSID:')[1]
+			if 'off/any' in str_essid:
+				essid = 'X'
+			print 'Interface: ' + str_interface + ', AP: ' + str_essid
 
-def searchESSID(list_interface):
-	list_essid = []
-	for str_interface in list_interface:
-		try:
-			list_result = os.popen('iwlist ' + str_interface + ' scanning').read()
-			list_result = list_result.split('\n')
-			for str_result in list_result:
-				if str_result.find('ESSID:') > 0:
-					list_essid.append(str_result.split('ESSID:')[1][1:-1])
-		except Exception:
-			pass
+def scanESSID(str_interface, bool_print = True):
+	file_in, file_out, file_error = os.popen3('iwlist ' + str_interface + ' scanning')
+	if file_error.read():
+		giveAlert('status')
+	else:
+		list_essid = []
+		list_result = file_out.read().split('\n')
+		for str_result in list_result:
+			if str_result.find('ESSID:') > 0:
+				list_essid.append(str_result.split('ESSID:')[1][1:-1])
+		if bool_print:
+			print 'Conntectable AP List:'
+		        print '========================================================='
+		       	for str_essid in list_essid:
+		                print str_essid
+	      		print '========================================================='
 	return list_essid
 
-def printESSID(list_essid):
-	print 'Conntectable AP List:'
-	print '========================================================='
-        for essid in list_essid:
-                print essid
-	print '========================================================='
-list_essid = searchESSID(getInterface())
-printESSID(list_essid)
+def changeESSID(str_interface, str_essid):
+	if str_essid in scanESSID(str_interface, False): 
+		file_in, file_out, file_error = os.popen3('iwconfig ' + str_interface + ' essid ' + str_essid)
+		if file_error.read():
+			giveAlert('input')
+		else:
+			file_in, file_out, file_error = os.popen3('iwconfig ' + str_interface)
+	        	str_result = file_out.read().split('\n')[0]
+			print file_out.read() 
+	                if 'off/any' in str_result.split('ESSID:')[1]:
+				giveAlert('input')
+			else:
+				print 'Success connection: ' + str_essid
+	else:
+		giveAlert('input')
--- Client/client.py
+++ Client/client.py
@@ -1,12 +1,32 @@
 import sys
 
 import help as Help
+import ap as AP
+
+def treatError():
+	if sys.argv[1] == 'scan':
+		AP.giveAlert('arg_s')
+	elif sys.argv[1] == 'change':
+		AP.giveAlert('arg_c')
+	else:
+		Help.giveAlert()	
 
 def parseMenu():
-        if sys.argv[1] == 'help':
-                Help.giveHelp()
-	else:
-		Help.giveAlert()
+	try:
+		if sys.argv[1] == 'help':
+	         	Help.giveHelp()
+                elif sys.argv[1] == 'iface':
+                        AP.getInterface()
+                elif sys.argv[1] == 'scan':
+                        AP.scanESSID(sys.argv[2])
+        	elif sys.argv[1] == 'change':
+                	AP.changeESSID(sys.argv[2], sys.argv[3])
+		else:
+			treatError()
+	except IndexError:
+		treatError()
+	except TypeError:
+		pass
 
 if __name__ == '__main__':
 	if len(sys.argv) == 1:
