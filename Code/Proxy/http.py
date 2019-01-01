eee0d4aa957f689136181dd39ae4e6edef859528
dreamline91 <dreamline91@naver.com>
2019-01-01 22:03:17 +0900

    Update http.py

--- Code/Proxy/http.py
+++ Code/Proxy/http.py
@@ -2,11 +2,11 @@ from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import urllib2
 import re
 
-p = re.compile(r'_[0-9]+kbit')
+p = re.compile(r'[0-9]+kbit')
 
 def changeBitrate(str_url):
 	if p.search(str_url):
-		str_before = p.search(str_url).group().split('_')[1]
+		str_before = p.search(str_url).group()
 		str_after = '50kbit'
 		return str_url.replace(str_before, str_after)
 	else:
@@ -20,8 +20,6 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 	str_url = changeBitrate(self.path)
-	print str_url
-	print
 	self._set_headers()
         self.wfile.write(urllib2.urlopen(str_url).read())
 
