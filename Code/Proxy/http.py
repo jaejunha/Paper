7a81ff24f38c8361bd5c624947d7be03a270bdeb
jaejunha <dreamline91@naver.com>
2019-01-16 01:14:57 +0900

    modify proxy code

--- Code/Proxy/http.py
+++ Code/Proxy/http.py
@@ -7,7 +7,7 @@ p = re.compile(r'[0-9]+kbit')
 def changeBitrate(str_url):
 	if p.search(str_url):
 		str_before = p.search(str_url).group()
-		str_after = '50kbit'
+		str_after = '100kbit'
 		return str_url.replace(str_before, str_after)
 	else:
 		return str_url
@@ -20,6 +20,7 @@ class Handler(BaseHTTPRequestHandler):
 
     def do_GET(self):
 	str_url = changeBitrate(self.path)
+	print str_url
 	self._set_headers()
         self.wfile.write(urllib2.urlopen(str_url).read())
 
--- Code/Proxy/proxy.py
+++ Code/Proxy/proxy.py
@@ -3,7 +3,6 @@ import sys
 import json
 
 import http as HTTP
-import rssi as RSSI
 
 def getIP():
 	f = open('server.json', 'r')
@@ -21,5 +20,4 @@ if __name__ == '__main__':
 		print 'Help > python proxy.py <wlan interface>' 
 	else:
 		dic_server = getIP()
-		RSSI.runRSSICollector(sys.argv[1], dic_server["SDN_Collector"])
 		openServer(dic_server["Proxy"]["PORT"])
