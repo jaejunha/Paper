67b14640d0c3db70aff4aa620aca7a7898a31a72
jaejunha <dreamline91@naver.com>
2019-01-17 05:37:17 +0900

    check timeout

--- Code/Proxy/http.py
+++ Code/Proxy/http.py
@@ -11,26 +11,30 @@ def changeBitrate(str_url):
 		socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		socket_server.connect(("141.223.65.54",7777))
 		socket_server.send(str_before.split('kbit')[0]+"\n")
-		str_msg = socket_server.recv(65535)
+		str_msg = socket_server.recv(1023)
 		str_after = str_msg.split(' ')[1].strip()
 		print "\nAdusted: "+str_after
+		socket_server.close()
 		return str_url.replace(str_before, str_after)
 	else:
 		return str_url 
 
 class Handler(BaseHTTPRequestHandler):
-    def _set_headers(self):
-        self.send_response(200)
-        self.send_header('Content-type', 'text/html')
-        self.end_headers()
+	def setup(self):
+    		BaseHTTPRequestHandler.setup(self)
+	 	self.request.settimeout(2)
+	def _set_headers(self):
+	       	self.send_response(200)
+	       	self.send_header('Content-type', 'text/html')
+	        self.end_headers()
 
-    def do_GET(self):
-	str_url = changeBitrate(self.path)
-	self._set_headers()
-	try:
-	       	self.wfile.write(urllib2.urlopen(str_url).read())
-	except:
-		pass
+	def do_GET(self):
+		str_url = changeBitrate(self.path)
+		self._set_headers()
+		try:
+		       	self.wfile.write(urllib2.urlopen(str_url).read())	
+		except:
+			pass
 
 def runServer(port):
 	HTTPServer(('',int(port)), Handler).serve_forever()
