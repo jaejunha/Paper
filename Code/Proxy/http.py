4f7774d0edde36a4f7836f2f325ea1c46922f135
jaejunha <dreamline91@naver.com>
2019-01-16 17:33:35 +0900

    pass 404 not found

--- Code/Proxy/http.py
+++ Code/Proxy/http.py
@@ -1,13 +1,18 @@
 from BaseHTTPServer import HTTPServer, BaseHTTPRequestHandler
 import urllib2
 import re
+import socket
 
 p = re.compile(r'[0-9]+kbit')
 
 def changeBitrate(str_url):
 	if p.search(str_url):
 		str_before = p.search(str_url).group()
-		str_after = '100kbit'
+		socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+		socket_server.connect(("141.223.65.54",7777))
+		socket_server.send(str_before.split('kbit')[0]+"\n")
+		str_msg = socket_server.recv(65535)
+		str_after = str_msg.split(' ')[1].strip()
 		print "\nAdusted: "+str_after
 		return str_url.replace(str_before, str_after)
 	else:
@@ -22,7 +27,10 @@ class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
 	str_url = changeBitrate(self.path)
 	self._set_headers()
-       	self.wfile.write(urllib2.urlopen(str_url).read())
+	try:
+	       	self.wfile.write(urllib2.urlopen(str_url).read())
+	except:
+		pass
 
 def runServer(port):
 	HTTPServer(('',int(port)), Handler).serve_forever()
