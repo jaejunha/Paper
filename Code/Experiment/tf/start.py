91afc9f79120e53b8285bc605ce046caeb5dec91
jaejunha <dreamline91@naver.com>
2019-04-14 11:36:44 +0900

    skeleton file

---
+++ Code/Experiment/tf/start.py
@@ -0,0 +1,13 @@
+﻿import sys
+
+if __name__ == "__main__":
+	if len(sys.argv) is 1:
+		print("Please select the mode")
+	else:
+		arg = sys.argv[1]
+		if arg == "train":
+			print("Trainning mode")
+		elif arg == "run":
+			print("Running mode")
+		else:
+			print("Please choose the correct mode")
